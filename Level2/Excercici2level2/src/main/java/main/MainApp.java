3d5c860cba79fe718a57fa3d9aa55f4b994140ed
JordiCasas87 <Jordicasas87@gmail.com>
2025-10-16 13:15:20 +0200

    ejercicio 1 level2, finalizado

--- Level2/Excercici2level2/src/main/java/main/MainApp.java
+++ Level2/Excercici2level2/src/main/java/main/MainApp.java
@@ -7,11 +7,6 @@ public class MainApp {
 
     public static void main(String[] args) {
 
-/*
- Exercici 1
-Modifica l'exercici anterior de manera que un dels arguments del mètode genèric no sigui genèric.
- */
-
         Person person1 = new Person("Jordi","Casas",38);
 
         System.out.println();
