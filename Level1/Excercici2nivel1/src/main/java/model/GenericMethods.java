59c99fda7fe3a65ad2cc00ea536321b3983fbacc
JordiCasas87 <Jordicasas87@gmail.com>
2025-10-16 13:00:11 +0200

    fix: Ejercicio2 level1, completo

--- Level1/Excercici2nivel1/src/main/java/model/GenericMethods.java
+++ Level1/Excercici2nivel1/src/main/java/model/GenericMethods.java
@@ -2,8 +2,6 @@ package model;
 
 public class GenericMethods <T> {
 
-    // si usas 3 parametros con 3 tipos genericos distintos debe de quedar reflejado tb en la delcaracion del metodo.
-
     public static <T,V,U> void ShowParameters (T parameter1, V parameter2, U parameter3){
         System.out.println("Estos son tus parametros: "+parameter1+", "+parameter2+"- "+parameter3+"");
     }
